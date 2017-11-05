7dc62c5cb3f509fecc15977484e4fa2d5dbe5119
wedwer666 <mitriucmaria@gmail.com>
2017-11-05 22:16:54 +0200

    Some changes 1

--- app/src/main/java/com/example/pam/webservice/RequestAnalyze.java
+++ app/src/main/java/com/example/pam/webservice/RequestAnalyze.java
@@ -2,14 +2,27 @@ package com.example.pam.webservice;
 
 import android.support.v7.app.AppCompatActivity;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by Maria on 03.11.2017.
  */
 
 public class RequestAnalyze extends AppCompatActivity {
 
-    //rejex file to extract filess
-    public extract ExtractInfo {
+    private String url = "https://www.orangetext.md/";
 
+    //rejex file to extract filess
+    public Captcha extractCaptcha()
+    {
+        StringBuilder sb = new StringBuilder("");
+
+        try {
+            URL requestUrl = new URL(url);
+
+            HttpURLConnection connection = (HttpURLConnection) requestUrl.openConnection();
+
+            connection.setRequestMethod("GET");
     }
 }
---
+++ app/src/main/java/com/example/pam/webservice/getInfo.java
@@ -0,0 +1,27 @@
+package com.example.pam.webservice;
+
+/**
+ * Created by Maria on 05.11.2017.
+ */
+
+//CLASS TO RECEIVE INFORMATION FOR REQUEST TO ORANGE.MD SITE
+public class getInfo {
+    public String getdelacine()
+    {
+        return getdelacine();
+    }
+    public String getnrdetelefon()
+    {
+        return getnrdetelefon();
+    }
+    public String getMesaj()
+    {
+        return getMesaj();
+    }
+    public String getCapthca()
+    {
+        return getCapthca();
+    }
+
+
+}
